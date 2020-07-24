9ce0c3a64a69c861f49b8be2810168efbe0620f4
haojie <haojie.d.yuan@gmail.com>
2020-07-24 08:57:52 +0800

    add support for gray scale images.

--- visualize.py
+++ visualize.py
@@ -4,13 +4,25 @@ from PIL import Image
 import numpy as np
 from torchvision import transforms
 from DCT import DCTTransform
+import os
 
 import pdb
 
 def save_img_tensor(x, name):
+    if x.shape[1] == 3:
+        x_numpy = x.detach().cpu().squeeze(0).permute(1,2,0).numpy()
+    elif x.shape[1] == 1:
+        x_numpy = x.detach().cpu().squeeze(0).squeeze(0).numpy()
+    else:
+        assert False, "Unkown image shape: {}".format(x.shape)
     x_numpy = x.detach().cpu().squeeze(0).permute(1,2,0).numpy()
     x_numpy = x_numpy*255
     im = Image.fromarray(np.uint8(x_numpy))
+    if not os.path.exists(os.path.dirname(name)):
+        try:
+            os.makedirs(os.path.dirname(name))
+        except:
+            pass
     im.save(name, 'PNG')
 
 
@@ -28,6 +40,7 @@ if __name__ == '__main__':
     dct_data = transform.dct_batch(x)
     idct_x = transform.idct_batch(dct_data)
     save_img_tensor(idct_x, 'idct.png')
+
 
     x_low, x_high = transform.dct_split(x, ratio=0.5, to_rgb=True)
     save_img_tensor(x_low+x_high, 'low+high.png')
